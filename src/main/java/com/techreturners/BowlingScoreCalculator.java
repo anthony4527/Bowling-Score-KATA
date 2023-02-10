479e5d2fd9238f258512581460b4ad2d7b52cd0b
AnthonyLee <anthony@calglobal.com>
2023-02-11 03:13:46 +0800

    add code for 2nd test case of all strikes

--- src/main/java/com/techreturners/BowlingScoreCalculator.java
+++ src/main/java/com/techreturners/BowlingScoreCalculator.java
@@ -2,19 +2,67 @@ package com.techreturners;
 
 public class BowlingScoreCalculator {
 
-    public int calculate (String pinRecord) {
-        int[] scoreForFrame= {0,0,0,0,0,0,0,0,0,0};
+    private final int fullScoreforThrow = 10;
+    private final int MaxFrame = 10;
+
+    public int calculate(String pinRecord) {
+        int[] scoreForFrame = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int resultScore = 0;
-        String[] pinforThrow;
+        String[] pinsforFrame;
+        boolean currentStrike = false;
+        boolean previousStrike = false;
+        int tempScore = 0;     //temp value before adjustment
 
         // split the input string by space to get the pin for each throw
         // sum the pin for each throw and add to the scoreForFrame
-        pinforThrow = pinRecord.split(" ");
-        for (int i=0; i < pinforThrow.length ; i++ ){
-            scoreForFrame[i] = Character.getNumericValue(pinforThrow[i].charAt(0)) + Character.getNumericValue(pinforThrow[i].charAt(1));
-            resultScore += scoreForFrame[i];
-        }
+        pinsforFrame = pinRecord.split(" ");
+        //System.out.println(pinsforFrame.length);
 
+        for (int i = 0; i < pinsforFrame.length; i++) {
+            tempScore = 0;
+            // if pinsforFrmae is 'x', score is 10; make a note for the next two throws for add
+            if (pinsforFrame[i].equals("x")) {
+                tempScore += fullScoreforThrow;
+                currentStrike = true;
+
+            } else {
+                tempScore = Character.getNumericValue(pinsforFrame[i].charAt(0)) + Character.getNumericValue(pinsforFrame[i].charAt(1));
+            }
+            //check if this is more than 10 frames, the score should be added to the last frame
+            //otherwise put tempScore to current frame
+            if (i< MaxFrame){
+                scoreForFrame[i] += tempScore;
+                if ((i > 0) && (pinsforFrame[i - 1].equals("x"))) {
+                    scoreForFrame[i - 1] += tempScore;
+                }
+                if ((i>1) && (pinsforFrame[i-2].equals("x")) && (pinsforFrame[i].length() ==1)) {
+                    scoreForFrame[i - 2] += tempScore;
+                }
+            } else {
+                // if this frame has one roll, add to 10th and 9th if each is a strike
+                if (pinsforFrame[i].length() == 1) {
+                    scoreForFrame[MaxFrame-1] += tempScore;
+                    if ((pinsforFrame[MaxFrame-2].equals("x")) && (i == MaxFrame)) {
+                        scoreForFrame[MaxFrame-2] += tempScore;
+                    }
+                }
+                else { //add two rolls to the last Frame
+                    scoreForFrame[MaxFrame-1] += tempScore;
+                    // if 9th frame is also a strike, add first roll to 9th frame too
+                    scoreForFrame[MaxFrame-2] += Character.getNumericValue(pinsforFrame[i].charAt(0));
+                }
+
+
+
+            }
+
+
+        } // end for all pinRecord
+
+        for (int i = 0; i < scoreForFrame.length; i++) {
+            resultScore += scoreForFrame[i];
+            System.out.println("final score for "+i+" is "+ scoreForFrame[i]);
+        }
         return resultScore;
     }
 }
