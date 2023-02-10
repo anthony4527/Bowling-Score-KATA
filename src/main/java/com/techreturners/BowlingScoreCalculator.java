27a83885427765e59465ca7c3aff742ca690dd31
AnthonyLee <anthony@calglobal.com>
2023-02-11 05:49:34 +0800

    correct spare case bonus addition to previous frame

--- src/main/java/com/techreturners/BowlingScoreCalculator.java
+++ src/main/java/com/techreturners/BowlingScoreCalculator.java
@@ -6,13 +6,14 @@ public class BowlingScoreCalculator {
 
     private final int fullScoreforThrow = 10;
     private final int MaxFrame = 10;
+    private final char Spare = '/';
+    private final char Missed = '-';
 
     public int calculate(String pinRecord) {
         int[] scoreForFrame = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int resultScore = 0;
         String[] pinsforFrame;
         int tempScore = 0;     //temp value before adjustment
-        boolean spareBonus = false;
 
         // split the input string by space to get the pin for each throw
         // sum the pin for each throw and add to the scoreForFrame
@@ -20,26 +21,23 @@ public class BowlingScoreCalculator {
 
         for (int i = 0; i < pinsforFrame.length; i++) {
             tempScore = 0;
-            // if pinsforFrmae is 'x', score is 10; make a note for the next two throws for add
+            // if pinsforFrmae is 'x', score is 10;
             if (pinsforFrame[i].equals("x")) {
                 tempScore += fullScoreforThrow;
-
             } else  {
                 // if pin record has a '-', ignore it and only use the numeric
                 // if pin record has a '/', score is 10 and set a flag for next throw to add
                 for (int j=0; j< pinsforFrame[i].length(); j++ ){
                     switch (pinsforFrame[i].charAt(j)){
-                        case '-':
+                        case Missed:
                             break;
-                        case '/':   // this will always come as the 2nd value; if the 1st roll knocks all pin, then it will have been a strike
-                            tempScore = 10;
-                            spareBonus = true;
+                        case Spare:   // this will always come as the 2nd value; if the 1st roll knocks all pin, then it will have been a strike
+                            tempScore = fullScoreforThrow;
                             break;
                         default:
                             tempScore +=Character.getNumericValue(pinsforFrame[i].charAt(j));
                     }
                 }
-
             }
             //check if this is more than 10 frames, the score should be added to the last frame
             //otherwise put tempScore to current frame
@@ -47,20 +45,23 @@ public class BowlingScoreCalculator {
                 scoreForFrame[i] += tempScore;
                 if ((i > 0) && (pinsforFrame[i - 1].equals("x"))) {
                     scoreForFrame[i - 1] += tempScore;
+                } else {
+                    // check if previous frame is a spare and then add bonus
+                    if ((i>0) && (pinsforFrame[i-1].charAt(1) == Spare)){
+                        if (pinsforFrame[i].length() ==1){
+                            scoreForFrame[i-1] += tempScore;
+                        } else {
+                            scoreForFrame[i-1] += Character.getNumericValue(pinsforFrame[i].charAt(0)) ;
+                        }
+                    }
                 }
+                //if current throw is 2nd roll after strike, add the score the previous strike frame
                 if ((i>1) && (pinsforFrame[i-2].equals("x")) && (pinsforFrame[i].length() ==1)) {
                     scoreForFrame[i - 2] += tempScore;
                 }
-                // add bonus to previous frame if a spare bonus has been on
-                if ((i>0) && (spareBonus == true)){
-                    if (pinsforFrame[i].length() ==1){
-                        scoreForFrame[i-1] += tempScore;
-                    } else {
-                        scoreForFrame[i-1] += Character.getNumericValue(pinsforFrame[i].charAt(0)) ;
-                    }
 
-                }
             } else {
+                // add the score for additional frame after the 10th
                 // if this frame has one roll, add to 10th for strike or spare
                 // if 9th is also a strike, add to 10th frame too
                 if (pinsforFrame[i].length() == 1) {
@@ -74,11 +75,7 @@ public class BowlingScoreCalculator {
                     // if 9th frame is also a strike, add first roll to 9th frame too
                     scoreForFrame[MaxFrame-2] += Character.getNumericValue(pinsforFrame[i].charAt(0));
                 }
-
-
-
             }
-
 
         } // end for all pinRecord
 
